7bcf7d1b878c3cb8d75ee1cdbc374d8c34cf5a02
BlooDZeNiX <cesarmsfelipe@gmail.com>
2022-05-10 17:46:16 +0200

    image

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -122,12 +122,9 @@ class AuthController extends Controller
             'file' => 'required|image|mimes:jpg,jfif,png,jpeg,gif,svg|max:2048|dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000',
         ]);
         $dir = 'assets/images/';
-        $absolutePath = public_path($dir);
+
         $fileName = time() . '.' . $request->file->getClientOriginalExtension();
         $relativePath = $dir . $fileName;
-        if (!File::exists($absolutePath)) {
-            File::makeDirectory($absolutePath, 0755, true);
-        }
         $request->file->move($relativePath . $fileName);
 
         User::where("id", $request->id)->update(['imageUrl' => $fileName]);
