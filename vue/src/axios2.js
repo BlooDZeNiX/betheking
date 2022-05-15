5078daf9eeb44c104d72beb7debd397278bcf964
BlooDZeNiX <cesarmsfelipe@gmail.com>
2022-05-15 13:58:42 +0200

    image fixes

--- vue/src/axios2.js
+++ vue/src/axios2.js
@@ -11,7 +11,7 @@ axiosClient2.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.state.user.token}`
     config.headers.common.Accept = 'multipart/form-data'
     config.headers['Access-Control-Allow-Origin'] = "*";
-    config.headers['Access-Control-Allow-Credentials'] = "true";
+    config.headers['Content-Type'] = "application/json";
     config.headers['Access-Control-Request-Method'] = "POST";
     return config;
 })
