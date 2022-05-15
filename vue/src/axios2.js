3bbf4036d98093dc49e96972786d229f11f1178b
BlooDZeNiX <cesarmsfelipe@gmail.com>
2022-05-15 14:15:55 +0200

    axios changes

--- vue/src/axios2.js
+++ vue/src/axios2.js
@@ -10,10 +10,10 @@ const axiosClient2 = axios.create({
 axiosClient2.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.state.user.token}`
     config.headers.common.Accept = 'multipart/form-data'
-    config.headers['Access-Control-Allow-Origin'] = "*";
-    config.headers['Access-Control-Allow-Credentials'] = "true";
-    config.headers['Content-Type'] = "application/json";
-    config.headers['Access-Control-Request-Method'] = "GET,HEAD,OPTIONS,POST,PUT";
+    config.headers.common['Access-Control-Allow-Origin'] = "*";
+    config.headers.common['Access-Control-Allow-Credentials'] = "true";
+    config.headers.common['Content-Type'] = "application/json";
+    config.headers.common['Access-Control-Request-Method'] = "GET,HEAD,OPTIONS,POST,PUT";
     console.log(config)
     return config;
 })
